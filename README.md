dfc937b1c0daf9ebef202097df9881cdc5300dd4
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2021-07-01 13:36:03 +0530

    final

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # socket_project_directory
-this repo consist of projects using sockets and there implementation
+
+## Overview 
+> in this we divide this project in 2 modules `client` and `server`
+
+### Client Side OverView 
+
+1. at first we do setup ton listen for events on give url so for this first we use socket client library
+2. then after successful connection we will set the events which we want to listen to perform some action. so `on` function is there in socket so we listen for events `output` , `cleared` and `status`. in output we list down all the messages , in cleared we list set the status text to cleared.
+3. at client side we also use emit function to push the event towards backend. so `emit` for `input` and `clear`. on input we sent the message payload to backend and on clear we send msg to backend to clear the chat history
+
+### Server Side OverView
+
+1. in this first we create a listner at specified port by using `express` framework and pass that listener into socket `listen` function.
+2. then we connect to mongo db on specified `uri` by creating a mongo client and then specify the db to use and create a collection and find for the chat history in the db.
+3. now using socket `emit` function emit the event by name `output` and send data . we use to `on` in backend side `input` and `clear`, for input we save the data as new document and on clear we clear the db.
+
+
+### How To Run This Project
+## prerequisite
+1. install node on your system
+## How to run
+1. clone this repo and cd to repo/db_sync_chat_app
+2. cd to server and run `npm i` and then start the server by `npm start`.
+3. cd .. and move to client folder by cd client and open index.html in browser
+
+> client output
+
+![image](./resources/output.png)
+
+
+
+
+
+
+
+
+
--- db_sync_chatapp/server/server.js
+++ db_sync_chatapp/server/server.js
@@ -1,69 +1,70 @@
-const mongo = require('mongodb').MongoClient;
-const client_ = require('socket.io').listen(4000).sockets;
-const uri = 'mongodb://localhost:27017';
+const mongo = require("mongodb").MongoClient;
+const express = require('express');
+const app = express();
+const port = 4000;
+const server =  app.listen(port);
+const client_ = require('socket.io').listen(server);
+const uri = "mongodb://localhost:27017";
 
-//connect to mongodb
-mongo.connect(uri,{ useUnifiedTopology: true } , function(err,client){
-
-    if(err)
-    throw err;
-
-    console.log('connect to Mongo .');
-    //db is test db
-
-    //connect to socket io
-    client_.on("connection", (socket)=>{
-        
-    let db = client.db('chatDb')
-    let collection = db.collection('chatCollection');
-        //Info of the database 
-        console.log(`DataBase is ${db.databaseName} and Collection is ${collection.collectionName}`);
-        //send status 
-        sendStatus =  function(s){
-            socket.emit('status',s);
-        }
-        
-        //get chats from mongo collection 
-        collection.find().limit(100).sort({_id:1}).toArray(function(err,res){
-            if(err)
-            throw err;
-            //emit the message
-            socket.emit("output", res);
-        })
-
-
-        //handle the input event
-        socket.on('input',function(data){
-            let { name ,message} = data;
-
-            //check for name and message
-            if(name == '' || message =='')
-            sendStatus('Body is Empty .')
-            else
-            collection.insertOne(data, function(){
-                client_.emit("output",[data]);
-
-                //sendStatus object
-                sendStatus({
-                    message:"Message Sent .",
-                    clear: true
-
-                })
-
-            })
-        })
-
-        //handle clear 
-        socket.on("clear",function(data){
-            collection.remove({},function(){
-                    //emit clear
-                    socket.emit('cleared');
-            })
-            
-            
-        })
-
-
-    })
-
+app.get('/health',(req,res)=>{
+    res.json({'msg':'server is up and running'});
 })
+//connect to mongodb
+mongo.connect(uri, { useUnifiedTopology: true }, function (err, client) {
+  if (err) throw err;
+  console.log('connected to mongodb');
+  //connect to socket io
+  client_.on("connection", (socket) => {
+    let db = client.db("chatDb");
+    let collection = db.collection("chatCollection");
+    console.log('client connected to socket server');
+    //Info of the database
+    // console.log(
+    //   `DataBase is ${db.databaseName} and Collection is ${collection.collectionName}`
+    // );
+    //send status
+    sendStatus = function (s) {
+      socket.emit("status", s);
+    };
+
+    //get chats from mongo collection
+    collection
+      .find()
+      .limit(100)
+      .sort({ _id: 1 })
+      .toArray(function (err, res) {
+        if (err) throw err;
+        //emit the message
+        socket.emit("output", res);
+      });
+
+    //handle the input event
+    socket.on("input", function (data) {
+      let { name, message } = data;
+
+      //check for name and message
+      if (name == "" || message == "") sendStatus("Body is Empty .");
+      else
+        collection.insertOne(data, function () {
+          client_.emit("output", [data]);
+
+          //sendStatus object
+          sendStatus({
+            message: "Message Sent .",
+            clear: true,
+          });
+        });
+    });
+    //handle disconnect event
+    socket.on('disconnect', function() {
+        console.log('client disconnected');
+    });
+    //handle clear
+    socket.on("clear", function (data) {
+      collection.remove({}, function () {
+        //emit clear
+        socket.emit("cleared");
+      });
+    });
+  });
+});
